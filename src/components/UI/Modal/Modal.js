25ddb93ad4c6d3b69c99763d39530098bcc9899a
Kevin Yang <kkaiyang94@gmail.com>
2019-11-14 15:37:52 -0800

    Add styling to Modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 import classes from './Modal.module.css';
 
-const Modal = props => (
-  <div className={classes.Modal}>
-    {props.children}
+const Modal = ({ show, children }) => (
+  <div
+    className={classes.Modal}
+    style={{
+      transform: show ? 'translateY(0)' : 'translateY(-100vh)',
+      opacity: show ? '1' : '0'
+    }}
+  >
+    {children}
   </div>
 );
 
