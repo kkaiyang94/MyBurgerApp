8f9634ebfb37bfc181b93633bce0c3a27f345a4e
Kevin Yang <kkaiyang94@gmail.com>
2019-11-14 14:13:52 -0800

    Implement Dynamically Building a burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,24 +2,28 @@ import React from 'react';
 
 import classes from './Burger.module.css';
 
-// import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const Burger = props => {
+const Burger = ({ ingredients }) => {
+
+  let transformedIngredients = Object.keys(ingredients)
+    .map(ingredient => {
+      return [...Array(ingredients[ingredient])].map((_, idx) => {
+        return <BurgerIngredient key={ingredient + idx} type={ingredient} />
+      })
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Start adding ingredients!</p>
+  }
   return (
     <div className={classes.Burger}>
-      <BurgerIngredient type='bread-top' />
-      <BurgerIngredient type='cheese' />
-      <BurgerIngredient type='meat' />
-      <BurgerIngredient type='cheese' />
-      <BurgerIngredient type='meat' />
-      <BurgerIngredient type='cheese' />
-      <BurgerIngredient type='meat' />
-      <BurgerIngredient type='cheese' />
-      <BurgerIngredient type='meat' />
-      <BurgerIngredient type='cheese' />
-      <BurgerIngredient type='meat' />
-      <BurgerIngredient type='bread-bottom' />
+      <BurgerIngredient type={'bread-top'} />
+      {transformedIngredients}
+      <BurgerIngredient type={'bread-bottom'} />
     </div>
   )
 }
