f5a4f9c039293f2824da3500bca2475b80aedcb6
Kevin Yang <kkaiyang94@gmail.com>
2019-11-14 17:28:15 -0800

    Add Toolbar Component to Layout and adjust css to fit Toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+
 import classes from './Layout.module.css';
 
 const Layout = props => (
   <Fragment>
-    <div>Placeholder: Toolbar, Sidebar, Backdrop</div>
+    <Toolbar />
     <main className={classes.Content}>{props.children}</main>
   </Fragment>
 )
