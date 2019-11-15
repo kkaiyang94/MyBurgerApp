d4502c6487c8457018e8e5fab727cc77fc0dff83
Kevin Yang <kkaiyang94@gmail.com>
2019-11-14 18:19:06 -0800

    Initialize NavigationItems and placeholder routes

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import NavigationItem from './NavigationItem/NavigationItem';
+
+import classes from './NavigationItems.module.css';
+
+const NavigationItems = props => (
+  <ul className={classes.NavigationItems}>
+    <NavigationItem link="/" active>Burger Builder</NavigationItem>
+    <NavigationItem link="/">Check Out</NavigationItem>
+  </ul>
+)
+
+export default NavigationItems;
