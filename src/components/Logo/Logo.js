2d5be2bef29964a6a59860d1db5430fe0c003b27
Kevin Yang <kkaiyang94@gmail.com>
2019-11-18 18:08:54 -0800

    Change default Logo CSS

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ import classes from './Logo.module.css';
 
 const Logo = props => (
   <div className={classes.Logo}>
-    <img src={burgerLogo} alt='BurgerImage'/>
+    <img src={burgerLogo} alt='BurgerImage' />
   </div>
 );
 
