6a868a3544ac941cbdeb141e2c9b88f2462b90c1
Kevin Yang <kkaiyang94@gmail.com>
2019-11-19 17:39:21 -0800

    Update Modal to have shouldComponentUpdate to optimize re-rendering

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Backdrop from '../Backdrop/Backdrop';
 
 import classes from './Modal.module.css';
 
-const Modal = ({ show, children, modalClosed }) => (
-  <>
-    <Backdrop
-    show={show}
-    clicked={modalClosed}
-    />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: show ? '1' : '0'
-      }}
-    >
-      {children}
-    </div>
-  </>
-);
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    const { show } = this.props;
+
+    return nextProps.show !== show;
+  }
+  render() {
+    const { show, children, modalClosed } = this.props;
+    return (
+      <>
+        <Backdrop
+          show={show}
+          clicked={modalClosed}
+        />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: show ? '1' : '0'
+          }}
+        >
+          {children}
+        </div>
+      </>
+    )
+  }
+};
 
 export default Modal;
