1429c7262cbc7df95fcd742cc8b3371013c2f61c
Kevin Yang <kkaiyang94@gmail.com>
2019-11-19 16:51:30 -0800

    Add methods for opening and closing SideDrawer and fix css

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,17 +6,33 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.module.css';
 
 class Layout extends Component {
+  state = {
+    showSideDrawer: false,
+  }
+
+  closeSideDrawerHandler = () => {
+    this.setState({ showSideDrawer: false })
+  }
+
+  sideDrawerToggle = () => {
+    this.setState((prevState) => {
+      return { showSideDrawer: !prevState.showSideDrawer }
+    })
+  }
+
   render() {
     const { children } = this.props;
+    const { showSideDrawer } = this.state;
 
     return (
       <>
-        <Toolbar />
-        <SideDrawer />
+        <Toolbar drawerToggleClicked={this.sideDrawerToggle}/>
+        <SideDrawer open={showSideDrawer} close={this.closeSideDrawerHandler} />
         <main className={classes.Content}>{children}</main>
       </>
     )
   }
 }
+
 
 export default Layout;
