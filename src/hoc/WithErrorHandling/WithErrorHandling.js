bbe8699327b4d29c5b4f2cebfc4f0e5a00e380a5
Kevin Yang <kkaiyang94@gmail.com>
2020-02-20 13:24:30 -0800

    Add error handling component

---
+++ src/hoc/WithErrorHandling/WithErrorHandling.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+import Modal from '../../components/UI/Modal/Modal';
+
+const WithErrorHandling = (WrappedComponent, axios) => {
+  return class extends Component {
+    state = {
+      error: false
+    }
+    componentWillMount() {
+      this.reqInterceptor = axios.interceptors.request.use(req => {
+        this.setState({ error: null })
+        return req;
+      })
+      this.resInterceptor = axios.interceptors.response.use(res => res, error => {
+        this.setState({ error: error })
+      })
+    }
+    componentWillUnmount() {
+      axios.interceptors.response.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
+    }
+    clearError = () => {
+      this.setState({ error: null });
+    }
+
+    render() {
+      return (
+        <>
+          <Modal show={this.state.error} modalClosed={this.clearError}>
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrappedComponent {...this.props}/>
+        </>
+      )
+    }
+  }
+}
+
+export default WithErrorHandling;
