004df7a618671f0c18b32d4e8f3e8d8d67213343
Kevin Yang <kkaiyang94@gmail.com>
2020-02-20 13:23:33 -0800

    Include updating on children change

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -6,9 +6,9 @@ import classes from './Modal.module.css';
 
 class Modal extends Component {
   shouldComponentUpdate(nextProps, nextState) {
-    const { show } = this.props;
+    const { show, children } = this.props;
 
-    return nextProps.show !== show;
+    return nextProps.show !== show || nextProps.children !== children;
   }
   render() {
     const { show, children, modalClosed } = this.props;
