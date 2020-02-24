52f939d21a6814dc9b1d15f034c28b42fc0433bb
Kevin Yang <kkaiyang94@gmail.com>
2020-02-24 00:22:33 -0800

    Remove mock data, add event handlers, add Route to get to customer information form

--- src/containers/CheckOut/CheckOut.js
+++ src/containers/CheckOut/CheckOut.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import CheckOutSummary from '../../components/Order/CheckOutSummary/CheckOutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
   state = {
-    ingredients: {
-      salad: 1,
-      cheese: 1,
-      bacon: 1,
-      meat: 1,
+    ingredients: null,
+    totalPrice: 0
+  }
+  componentWillMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const orderedIngredients = {};
+    let price = 0;
+
+    for (let param of query.entries()) {
+      if (param[0] === 'totalPrice') price = +param[1];
+      else orderedIngredients[param[0]] = +param[1];
     }
+
+    this.setState({ ingredients: orderedIngredients, totalPrice: price });
+  }
+  checkOutCancelHandler = () => {
+    this.props.history.goBack();
+  }
+  checkOutContinueHandler = () => {
+    this.props.history.push('/checkout/contact-data');
   }
   render() {
-
+    const { ingredients, totalPrice } = this.state;
     return (
       <div>
-        <CheckOutSummary ingredients={this.state.ingredients}/>
+        <CheckOutSummary 
+          ingredients={this.state.ingredients} 
+          checkOutCancel={this.checkOutCancelHandler} 
+          checkOutContinue={this.checkOutContinueHandler}/>
+        <Route path={this.props.match.path + '/contact-data'} render={(props) => (<ContactData totalPrice={totalPrice} ingredients={ingredients} {...props}/>)}/>
       </div>
     )
   }
